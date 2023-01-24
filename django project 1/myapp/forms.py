0701792c2e026e23e7a9f6cb2f63a137cb8361da
franketi01 <francogm01@gmail.com>
2023-01-24 00:51:20 -0300

    Delete django project 1 directory

--- django project 1/myapp/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms   # se agrego este script para agregar forms
-
-class CreateNewTask(forms.Form):
-    title = forms.CharField(label="Titulo de tarea", max_length=200,widget=forms.TextInput(attrs={"class":"input"}))
-    description = forms.CharField(label="Descripcion de la tarea",widget=forms.Textarea )
-
-class CreateNewProject(forms.Form):
-    name = forms.CharField(label="Nombre del Proyecto", max_length=200,widget=forms.TextInput(attrs={"class":"input"}))
-    
